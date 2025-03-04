6b49e9933c2ede11c6931d166bac9319d9326ca5
malak <r783c257@wichita.edu>
2025-03-04 14:49:57 -0600

    Added references to missing files in .fsproj

--- obj/Debug/net8.0/GiraffeTodo.AssemblyInfo.fs
+++ obj/Debug/net8.0/GiraffeTodo.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("GiraffeTodo")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c5cea3ca69585778b8121fa3f75c472d0fcedb")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+901ebfb7f6215e0bba4fa58fc8ca2f1c6c9201a0")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("GiraffeTodo")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("GiraffeTodo")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
