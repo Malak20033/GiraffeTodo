5e30cb5ccd9a83b43a7996c2f80753270ecece81
Malak Shakhtour <malakshakhtour@gmail.com>
2025-03-05 00:35:09 -0600

    Fixed .fsproj file and added html code

---
+++ Models/Models.fs
@@ -0,0 +1,14 @@
+namespace MyApp.Models
+
+module Todo = 
+    type TodoItem = {
+        Id: int
+        Task: string
+        IsCompleted: bool
+    }
+
+    let createTodoItem id task =
+        { Id = id; Task = task; IsCompleted = false }
+
+    let completeTodoItem todo =
+        { todo with IsCompleted = true }
